1390f72a9c6063c709d724fbbf6d0f4b09272f43
YuluaOkrushko <yuluadmitrieva94@gmail.com>
2020-02-22 18:23:32 +0200

    js

--- goit-js-hw-01/JS/task-5.js
+++ goit-js-hw-01/JS/task-5.js
@@ -1,28 +1,28 @@
 let message;
 let answer = prompt("В какую страну необходима доставка?");
 let delivery;
-answer = answer[0].toUpperCase() + answer.slice(1).toLowerCase();
+answer = answer.toLowerCase();
 
 switch (answer) {
-  case "Китай":
+  case "китай":
     delivery = 100;
-    message = `Доставка в ${answer} будет стоить ${delivery} кредитов`;
+    message = `Доставка в Китай будет стоить ${delivery} кредитов`;
     break;
-  case "Чили":
+  case "чили":
     delivery = 250;
-    message = `Доставка в ${answer} будет стоить ${delivery} кредитов`;
+    message = `Доставка в Чили будет стоить ${delivery} кредитов`;
     break;
-  case "Австралия":
+  case "австралия":
     delivery = 170;
-    message = `Доставка в ${answer} будет стоить ${delivery} кредитов`;
+    message = `Доставка в Австралия будет стоить ${delivery} кредитов`;
     break;
-  case "Индия":
+  case "индия":
     delivery = 80;
-    message = `Доставка в ${answer} будет стоить ${delivery} кредитов`;
+    message = `Доставка в Индия будет стоить ${delivery} кредитов`;
     break;
-  case "Ямайка":
+  case "ямайка":
     delivery = 120;
-    message = `Доставка в ${answer} будет стоить ${delivery} кредитов`;
+    message = `Доставка в Ямайка будет стоить ${delivery} кредитов`;
     break;
   default:
     message = `В вашей стране доставка не доступна`;
