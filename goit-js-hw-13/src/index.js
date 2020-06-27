f4db2e66350fd888987d56e875c5342533d83462
YuluaOkrushko <yuluadmitrieva94@gmail.com>
2020-06-27 12:13:17 +0300

    hw-13

--- goit-js-hw-13/src/index.js
+++ goit-js-hw-13/src/index.js
@@ -5,13 +5,20 @@ import debounce from 'lodash.debounce';
 
 let inputText = document.querySelector('.input_text');
 let loadButton = document.querySelector('.load_button');
+
+
 let page = 1;
+let result;
 let search = async() => {
     page = 1;
     if (inputText.value) {
-        let result = await imgService(inputText.value, page);
+        result = await imgService(inputText.value, page);
         document.querySelector('.image_list').innerHTML = imgCard(result.hits);
-        loadButton.style.display = 'block';
+        if (result.hits.length > 0) {
+            loadButton.style.display = 'block'
+        } else {
+            loadButton.style.display = 'none'
+        }
     } else {
         document.querySelector('.image_list').innerHTML = '';
         loadButton.style.display = 'none';
@@ -24,10 +31,10 @@ let loadMore = async() => {
         .querySelector('.image_list')
         .insertAdjacentHTML('beforeend', imgCard(result.hits));
     window.scrollTo({
-        top: window.scrollY + window.screen.height,
+        top: (window.scrollY + window.screen.height) - loadButton.scrollHeight,
         left: 0,
         behavior: 'smooth',
-    });
+    })
 };
 loadButton.addEventListener('click', loadMore);
 inputText.addEventListener('input', debounce(search, 500));
